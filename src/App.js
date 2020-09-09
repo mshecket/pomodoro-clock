559cf466581f02bd5ce47873ee6ad06db4cc24b8
mshecket <mike@mikeshecket.com>
2020-09-08 23:05:18 -0400

    Improved responsiveness

--- src/App.js
+++ src/App.js
@@ -1,97 +1,106 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  
-  const [breakLength, setBreakLength] = React.useState(5)
-  const [sessionLength, setSessionLength] = React.useState(25)
-  const [timeLeft, setTimeLeft] = React.useState(25*60)
-  const [running, setRunning] = React.useState(false)
-  const [session, setSession] = React.useState(true)
-  
-  React.useEffect(() => { const clockTask = setTimeout(() => {
-    if (running === true && (timeLeft > 0))
-        setTimeLeft(timeLeft - 1)
-    if (timeLeft === 1) {
-      document.getElementById("beep").play()
-      setSession(!session)
-    }
-    if (timeLeft === 0) {
-      if (!session)
-        setTimeLeft(sessionLength * 60)
-      else
-        setTimeLeft(breakLength * 60)
-    }
-  },1000)
-  return () => { clearTimeout(clockTask) }
-},[timeLeft,running])
-  
-  
+  const [breakLength, setBreakLength] = React.useState(5);
+  const [sessionLength, setSessionLength] = React.useState(25);
+  const [timeLeft, setTimeLeft] = React.useState(25 * 60);
+  const [running, setRunning] = React.useState(false);
+  const [session, setSession] = React.useState(true);
+
+  useEffect(() => {
+    const clockTask = setTimeout(() => {
+      if (running === true && timeLeft > 0) setTimeLeft(timeLeft - 1);
+      if (timeLeft === 1) {
+        document.getElementById("beep").play();
+        setSession(!session);
+      }
+      if (timeLeft === 0) {
+        if (!session) setTimeLeft(sessionLength * 60);
+        else setTimeLeft(breakLength * 60);
+      }
+    }, 1000);
+    return () => {
+      clearTimeout(clockTask);
+    };
+  }, [timeLeft, running, breakLength, session, sessionLength]);
+
   function handleClick(event) {
-    event.preventDefault()
+    event.preventDefault();
     switch (event.target.id) {
       case "reset":
-        setBreakLength(5)
-        setSessionLength(25)
-        setTimeLeft(25*60)
-        setRunning(false)
-        setSession(true)
-        document.getElementById("beep").load()
-        break
+        setBreakLength(5);
+        setSessionLength(25);
+        setTimeLeft(25 * 60);
+        setRunning(false);
+        setSession(true);
+        document.getElementById("beep").load();
+        break;
       case "break-decrement":
-        setBreakLength(Math.max(breakLength - 1,1))
-        break
+        setBreakLength(Math.max(breakLength - 1, 1));
+        break;
       case "break-increment":
-        setBreakLength(Math.min(breakLength + 1,60))
-        break
+        setBreakLength(Math.min(breakLength + 1, 60));
+        break;
       case "session-decrement":
-        setSessionLength(Math.max(sessionLength - 1,1))
-        if (running === false)
-          setTimeLeft((sessionLength-1)*60)
-        break
+        setSessionLength(Math.max(sessionLength - 1, 1));
+        if (running === false) setTimeLeft((sessionLength - 1) * 60);
+        break;
       case "session-increment":
-        setSessionLength(Math.min(sessionLength + 1,60))
-        if (running === false)
-          setTimeLeft((sessionLength+1)*60)
-        break
+        setSessionLength(Math.min(sessionLength + 1, 60));
+        if (running === false) setTimeLeft((sessionLength + 1) * 60);
+        break;
       case "start_stop":
-        setRunning(running ? false : true)
-        break
+        setRunning(running ? false : true);
+        break;
       default:
-        break
+        break;
     }
   }
-  
+
   return (
-    <div id="clock">
-      <label id="break-label">
-        Break Length: {breakLength}
-      </label>
-      <br/>
-        <button id="break-decrement" onClick={handleClick}>-</button>
-        <button id="break-increment" onClick={handleClick}>+</button>
-      <br/>
-      <label id="session-label">
-        Session Length: {sessionLength}
-      </label>
-      <br/>
-        <button id="session-decrement" onClick={handleClick}>-</button>
-        <button id="session-increment" onClick={handleClick}>+</button>
-      
-      <br/>
-      
-      <label id="timer-label">{session ? "Session:" : "Break:"} {(""+Math.floor(timeLeft/60)).padStart(2,"0")+":"+(""+(timeLeft % 60)).padStart(2,"0")}
-      </label>
-      <br/>
-      <button id="start_stop" onClick={handleClick}>
-        {running ? "Stop" : "Start"}
-      </button>
-      <button id="reset" onClick={handleClick}>
-        Reset
-      </button>
-      <audio src="http://www.mikeshecket.com/beep.mp3" id="beep"></audio>
+    <div id="app">
+      <div id="clock">
+        <label id="break-label">Break Length: {breakLength}</label>
+        <br />
+        <button id="break-decrement" onClick={handleClick}>
+          -
+        </button>
+        <button id="break-increment" onClick={handleClick}>
+          +
+        </button>
+        <br />
+        <label id="session-label">Session Length: {sessionLength}</label>
+        <br />
+        <button id="session-decrement" onClick={handleClick}>
+          -
+        </button>
+        <button id="session-increment" onClick={handleClick}>
+          +
+        </button>
+
+        <br />
+
+        <label id="timer-label">
+          {session ? "Session:" : "Break:"}{" "}
+          {("" + Math.floor(timeLeft / 60)).padStart(2, "0") +
+            ":" +
+            ("" + (timeLeft % 60)).padStart(2, "0")}
+        </label>
+        <br />
+        <button id="start_stop" onClick={handleClick}>
+          {running ? "Stop" : "Start"}
+        </button>
+        <button id="reset" onClick={handleClick}>
+          Reset
+        </button>
+        <audio
+          src="http://pomodoroclock.mikeshecket.com/beep.mp3"
+          id="beep"
+        ></audio>
+      </div>
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
